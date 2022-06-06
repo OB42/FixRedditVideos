eb27958ab97b2d4d387e1a8620fb334dc3985da7
Otmane Benazzou <otmanebenazzou.pro+github@gmail.com>
2022-06-06 18:02:17 +0200

    add other resolutions as backup

--- FixRedditVideos.user.js
+++ FixRedditVideos.user.js
@@ -12,8 +12,15 @@ setInterval(() => {
                 const id = oldVideo.querySelector('source').src.split('.it/')[1].split('/')[0]
                 const player = oldVideo.parentNode
                 player.innerHTML =
-                    `<video controls onclick="event.stopPropagation();" preload="auto" src="https://v.redd.it/${id}/DASH_720.mp4" style="width:100%!important;"></video>`
-                    + `<audio preload="none" src="https://v.redd.it/${id}/DASH_audio.mp4"></audio>`
+
+                    `
+                    <video controls onclick="event.stopPropagation();" preload="auto" style="width:100%!important;">
+                        <source src="https://v.redd.it/${id}/DASH_1080.mp4">
+                        <source src="https://v.redd.it/${id}/DASH_480.mp4">
+                        <source src="https://v.redd.it/${id}/DASH_360.mp4">
+                        <source src="https://v.redd.it/${id}/DASH_240.mp4">
+</video>`
+                    + `< audio preload="none" src="https://v.redd.it/${id}/DASH_audio.mp4"></audio>`
                 let video = player.querySelector('video')
                 let audio = player.querySelector('audio')
                 video.onplay = function () {
@@ -23,7 +30,7 @@ setInterval(() => {
                 video.onpause = function () { audio.pause(); }
                 console.log(`Fixed the fucking reddit video with ID#`, id)
             } catch (err) {
-                console.warn('Error while fixing the fucking reddit videos, fuck reddit', oldVideo, i)
+                console.warn('Error while fixing the fucking reddit videos, fuck reddit', id, source)
             }
         })
 }, 42)

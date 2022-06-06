b4098baa525599066803922033efd56c298f6bc6
Otmane Benazzou <otmanebenazzou.pro+github@gmail.com>
2022-06-06 17:37:21 +0200

    Initial commit

---
+++ ChromeExtensionSourceCode/content.js
@@ -0,0 +1,22 @@
+setInterval(() => {
+    [].slice.call(document.querySelectorAll('[data-click-id="media"]'))
+        .filter(x => x.querySelector('video') && x.querySelector('source')).forEach((oldVideo, i) => {
+            try {
+                const id = oldVideo.querySelector('source').src.split('.it/')[1].split('/')[0]
+                const player = oldVideo.parentNode
+                player.innerHTML =
+                    `<video controls onclick="event.stopPropagation();" preload="auto" src="https://v.redd.it/${id}/DASH_720.mp4" style="width:100%!important;"></video>`
+                    + `<audio preload="none" src="https://v.redd.it/${id}/DASH_audio.mp4"></audio>`
+                let video = player.querySelector('video')
+                let audio = player.querySelector('audio')
+                video.onplay = function () {
+                    audio.currentTime = video.currentTime
+                    audio.play();
+                }
+                video.onpause = function () { audio.pause(); }
+                console.log(`Fixed the fucking reddit video with ID#`, id)
+            } catch (err) {
+                console.warn('Error while fixing the fucking reddit videos, fuck reddit', id, source)
+            }
+        })
+}, 42)
---
+++ FixRedditVideos.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name Fix Reddit Videos
+// @description Fix the buggy video player that don't load correctly on reddit.com(you can't change the volume or resolution for now, maybe I'll add it later)
+// @include *://*.reddit.*
+// @author Otmane Benazzou
+// @version     1
+// ==/UserScript==
+setInterval(() => {
+    [].slice.call(document.querySelectorAll('[data-click-id="media"]'))
+        .filter(x => x.querySelector('video') && x.querySelector('source')).forEach((oldVideo, i) => {
+            try {
+                const id = oldVideo.querySelector('source').src.split('.it/')[1].split('/')[0]
+                const player = oldVideo.parentNode
+                player.innerHTML =
+                    `<video controls onclick="event.stopPropagation();" preload="auto" src="https://v.redd.it/${id}/DASH_720.mp4" style="width:100%!important;"></video>`
+                    + `<audio preload="none" src="https://v.redd.it/${id}/DASH_audio.mp4"></audio>`
+                let video = player.querySelector('video')
+                let audio = player.querySelector('audio')
+                video.onplay = function () {
+                    audio.currentTime = video.currentTime
+                    audio.play();
+                }
+                video.onpause = function () { audio.pause(); }
+                console.log(`Fixed the fucking reddit video with ID#`, id)
+            } catch (err) {
+                console.warn('Error while fixing the fucking reddit videos, fuck reddit', id, source)
+            }
+        })
+}, 42)
